5e59c87fbe10cea7a1608fd3a056a1f16100914e
S000777 <s000777@tf.cn>
2026-08-04 15:58:23 +0800

    升级版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
             .ofPattern("yyMMddHH")
             .format(LocalDateTime.now())
             .toInt()
-        versionName = "1.0.4"
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
